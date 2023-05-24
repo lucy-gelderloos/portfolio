89f901657763750e2ecf483b70003ab969d7e998
Lucy Gelderloos <lucygel@gmail.com>
2023-05-24 16:50:28 -0700

    add & position logo in header; add favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
+import logoComplexPinkOrange from '../img/logo-complex-pink-orange.svg'
+
+
 const Header = () => {  
     return (            
     <header>
-      <div>
-        <img src="img/logo-complex-pink-orange.svg" alt="logo-complex-pink-orange" />
+      {/* thinking about one div w/cream bg, one w/ green bg, one on top of both with no bg so img can horizontally center */}
+      <div id="header-top"></div>
+      <div id="header-divider">
+        <img src={logoComplexPinkOrange} alt="logo-complex-pink-orange" id="header-logo" />
       </div>
+      <div id="header-bottom"></div>
     </header>
     ); 
   };
