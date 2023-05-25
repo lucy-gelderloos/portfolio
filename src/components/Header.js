a5f9f51dc08398a442ab6057b2fd52c70e9d03b9
Lucy Gelderloos <lucygel@gmail.com>
2023-05-24 17:10:28 -0700

    clean up header divs

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,11 @@ const Header = () => {
     return (            
     <header>
       {/* thinking about one div w/cream bg, one w/ green bg, one on top of both with no bg so img can horizontally center */}
-      <div id="header-top"></div>
+      {/* <div id="header-top"></div> */}
       <div id="header-divider">
         <img src={logoComplexPinkOrange} alt="logo-complex-pink-orange" id="header-logo" />
       </div>
-      <div id="header-bottom"></div>
+      {/* <div id="header-bottom"></div> */}
     </header>
     ); 
   };
